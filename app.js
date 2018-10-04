6050846f5e26b6ddbf4fbeed1302c22f8f73c40e
Jobi Mathai <jobi.mathai@gmail.com>
2018-10-03 20:32:46 -0700

    initial commit with .on click function

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //add database connection info:
-
 // Initialize Firebase
 var config = {
     apiKey: config.MY_KEY,
@@ -10,3 +9,37 @@ var config = {
     messagingSenderId: "351431078153"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  //Initial Values
+
+  var empName = "Chuck Norris";
+  var empRole = "CBO";
+  var empStartDate;
+  var empMnthWorked = 0;
+  var empMmnthRate = 0;
+  var empTotlBilled = 0;
+
+
+
+  //on click function
+
+    $("#submit-btn").on("click", function(event){
+        event.preventDefault();
+        var employee = $("#employee-name").val().trim();
+        var role = $("#employee-role").val().trim();
+        var startDate = $("#employee-startDate").val().trim();
+        var monthlyRate = parseInt($("#employee-MonthlyRate").val().trim());
+        
+         
+
+        database.ref().push({
+            employee: employee,
+            role: role,
+            monthsWorked: startDate,
+            monthlyRate: monthlyRate,
+            billedAmount: billedAmount,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+          });
+    })
