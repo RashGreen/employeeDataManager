06bd3dc01d82363c307a9f149b89b2bb0379c1a5
Jobi Mathai <jobi.mathai@gmail.com>
2018-10-03 19:19:08 -0700

    added database connection details

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+//add database connection info:
 
+// Initialize Firebase
+var config = {
+    apiKey: config.MY_KEY,
+    authDomain: "timesheet-app-65e60.firebaseapp.com",
+    databaseURL: "https://timesheet-app-65e60.firebaseio.com",
+    projectId: "timesheet-app-65e60",
+    storageBucket: "timesheet-app-65e60.appspot.com",
+    messagingSenderId: "351431078153"
+  };
+  firebase.initializeApp(config);
